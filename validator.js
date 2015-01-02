d74ba0c292865699227584749253420431640024
ejoseca <ejoseca@users.noreply.github.com>
2015-01-02 14:59:00 +0100

    Bug

--- validator.js
+++ validator.js
@@ -283,7 +283,7 @@ var Validator = function (rules, data, models) {
 				validator.models[parameters[0]].find({where: where})
 				.then(function(instance) {
 					if (instance) {
-						this.addFoundModel(attribute, instance);
+						validator.addFoundModel(attribute, instance);
 						resolve(false);
 					}
 					else {
